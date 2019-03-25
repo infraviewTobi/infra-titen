78801f9046ea7dbb2d1a72be68f67beecb3d7365
Tobias Heiser <tobias.heiser@infraview.net>
2019-03-25 08:11:24 +0100

    clean imports

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,4 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AppComponent} from '../app.component';
 import {RouterModule, Routes} from '@angular/router'
 
 import {PlayersComponent} from '../players/players.component';
